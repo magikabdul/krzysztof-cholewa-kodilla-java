bdc5da168e5a039b9f2a3dda9e877b525154e29d
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-11-08 19:43:55 +0100

    Calculator class and test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,15 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        if (Calculator.add(12, 28) == 12 + 28) { System.out.println("Test for class Calculator, method \"add\" - ok"); }
+        else { System.out.println("Test for class Calculator, method \"add\" - error"); }
+
+        if (Calculator.subtract(12, 2) == 12 - 2) { System.out.println("Test for class Calculator, method \"subtract\" - ok"); }
+        else { System.out.println("Test for class Calculator, method \"add\" - error"); }
     }
 }
