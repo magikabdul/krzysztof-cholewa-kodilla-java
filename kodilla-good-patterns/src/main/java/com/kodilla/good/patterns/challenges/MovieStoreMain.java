b6bfc1968de3d79bafec31deffd24bae999263e5
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-12-04 14:43:15 +0100

    Module 9.1 - fix no.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
@@ -1,9 +1,6 @@
 package com.kodilla.good.patterns.challenges;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.StringStack;
-
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MovieStoreMain {
 
@@ -11,10 +8,11 @@ public class MovieStoreMain {
 
         MovieStore movieStore = new MovieStore();
 
-        String singleStringWithExclamations =  movieStore.getMovies().values().stream()
-                .map(strings -> strings.get(0) + "!" + strings.get(1))
+         String singleStringWithExclamations = movieStore.getMovies().entrySet().stream()
+                .flatMap(stringListEntry -> stringListEntry.getValue().stream())
+                .map(s -> s.substring(0, s.length()))
                 .collect(Collectors.joining("!"));
 
-        System.out.println(singleStringWithExclamations);
+         System.out.println(singleStringWithExclamations);
     }
 }
