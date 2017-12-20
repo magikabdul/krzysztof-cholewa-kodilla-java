81c083a7914478e567b907bec8b6017789f59831
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2017-12-20 17:55:46 +0100

    Task 10.3 fix

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -13,26 +13,29 @@ public class BoardTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
 
         //When
-        int toDoListSize = context.getBean(Board.class).getToDoList().getTasks().size();  //getTaskList(Board.TASK_TYPE_TO_DO).getTasks().size();
-        int inProgressListSize = context.getBean(Board.class).getInProgressList().getTasks().size();    //getTaskList(Board.TASK_TYPE_IN_PROGRESS).getTasks().size();
-        int doneListSize = context.getBean(Board.class).getDoneList().getTasks().size();    //getTaskList(Board.TASK_TYPE_DONE).getTasks().size();
+        context.getBean(Board.class).getToDoList().addTask("New to do task");
+        context.getBean(Board.class).getInProgressList().addTask("New in progress task");
+        context.getBean(Board.class).getDoneList().addTask("New done task");
+
+        int toDoListSize = context.getBean(Board.class).getToDoList().getTasks().size();
+        int inProgressListSize = context.getBean(Board.class).getInProgressList().getTasks().size();
+        int doneListSize = context.getBean(Board.class).getDoneList().getTasks().size();
+
+        int indexOfNewTaskToDo = context.getBean(Board.class).getToDoList().getTasks().indexOf("New to do task");
+        String resultOfTaskToDo = context.getBean(Board.class).getToDoList().getTasks().get(indexOfNewTaskToDo).toString();
+
+        int indexOfNewTaskInProgress = context.getBean(Board.class).getInProgressList().getTasks().indexOf("New in progress task");
+        String resultOfTaskInProgress = context.getBean(Board.class).getInProgressList().getTasks().get(indexOfNewTaskInProgress).toString();
+
+        int indexOfNewTaskDone = context.getBean(Board.class).getDoneList().getTasks().indexOf("New done task");
+        String resultOfTaskDone = context.getBean(Board.class).getDoneList().getTasks().get(indexOfNewTaskDone).toString();
 
         //Then
-        Assert.assertEquals(5, toDoListSize);
-        Assert.assertEquals(10, inProgressListSize);
-        Assert.assertEquals(2, doneListSize);
-    }
-
-    @Test
-    public void testReadingEmptyTaskList() {
-        //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
-
-        //When @ Then
-        try {
-            context.getBean(Board.class).getToDoList().getTasks().size();
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-        }
+        Assert.assertEquals(6, toDoListSize);
+        Assert.assertEquals("New to do task", resultOfTaskToDo);
+        Assert.assertEquals(11, inProgressListSize);
+        Assert.assertEquals("New in progress task", resultOfTaskInProgress);
+        Assert.assertEquals(3, doneListSize);
+        Assert.assertEquals("New done task", resultOfTaskDone);
     }
 }
