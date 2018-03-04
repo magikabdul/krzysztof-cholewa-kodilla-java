cf0e233a77919bd6c38f8e64a2e0dc76311746bb
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2018-03-04 16:01:33 +0100

    Module 22.3 - Advanced queries

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StoredPostTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not Set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL updateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0, howMany);
+    }
+}
